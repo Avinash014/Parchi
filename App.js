39e9132ea91753285e602574a1e3f66cc49b085a
Avinash Kumar <avinash.kumar@cognizer.ai>
2021-06-20 22:55:19 +0530

    some code cleanup

--- App.js
+++ App.js
@@ -1,48 +1,9 @@
-import { StatusBar } from "expo-status-bar";
+
 import React from "react";
-import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
-import { Provider as StoreProvider } from "react-redux";
-import configureStore from "./app/store/configureStore";
-import { StyleSheet, Text, View } from "react-native";
-import Main from "./app/Main";
-import App1 from "./src/App"
-
-
-import Welcome from "./app/Screens/Welcome";
-// const store = configureStore();
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: "tomato",
-    accent: "yellow",
-  },
-};
+import Main from "./src/Main";
 
 export default function App() {
-  const store = configureStore();
-
-  const [userName, setUserName] = React.useState("");
-  React.useEffect(() => {
-    setUserName("Avinash");
-  });
-  console.log("app a");
-  var x = 1;
   return (
-    <StoreProvider store={store}>
-      <PaperProvider>
-        {/* <Main /> */}
-        <App1/>
-      </PaperProvider>
-    </StoreProvider>
+    <Main />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/Main.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Home from './containers/Home';
+import Conference from './containers/Conference';
+import Story from './containers/Story';
+import Sessions from './containers/Sessions';
+import Speakers from './containers/Speakers';
+import CreateOrder from './containers/CreateOrder';
+import FinalOrder from './containers/FinalOrder';
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import { Provider as StoreProvider } from "react-redux";
+import configureStore from "../app/store/configureStore";
+
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const Main = () => {
+  const store = configureStore();
+  return (
+    <StoreProvider store={store}>
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Create Order" component={CreateOrder} />
+            <Stack.Screen name="Final Order" component={FinalOrder} />
+            <Stack.Screen name="Conference" component={Conference} />
+            <Stack.Screen name="Story" component={Story} />
+            <Stack.Screen name="Sessions" component={Sessions} />
+            <Stack.Screen name="Speakers" component={Speakers} />
+          </Stack.Navigator>
+        </NavigationContainer>
+    </PaperProvider>
+    </StoreProvider>
+  );
+};
+
+export default Main;
+
