61bd9a31bd8bc54f021341a482f844f982a6e8a9
Avinash Kumar <avinash.kumar@cognizer.ai>
2021-06-21 01:30:49 +0530

    styling create order page

--- src/containers/CreateOrder.js
+++ src/containers/CreateOrder.js
@@ -1,23 +1,71 @@
 import React, {useState} from 'react';
-import {Text, FlatList, View, TextInput, Pressable, Image} from 'react-native';
+import {Text, FlatList, View, TextInput, Pressable, Image, Button, ScrollView} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {StyleSheet} from 'react-native';
 import {orders} from '../data/orders.json';
 // import styles from './styles/sharedStyles.js';
 import {Footer} from '../components/Footer';
 import {Header} from '../components/Header';
+import { IconButton, Colors,Avatar } from 'react-native-paper';
+
 const styles = StyleSheet.create({
+    container:{
+      padding:'2px',
+      backgroundColor: 'skyblue',
+      flex:1,
+      alignItems: "center",
+    },
+    orderContainer:{
+      width:'100%',
+    },
     orderRow : {
         width: '100%',
+        border:'1px solid black',
+        backgroundColor: 'white',
+        borderRadius:"5px",
         height: '50px',
-        backgroundColor: 'skyblue',
         flexDirection:'row',
+        alignItems: "center",
+        marginBottom:"2px",
+
     },
     inputItem :{
-        width:'60%'
+        width:'50%',
+        height:'75%',
+        padding:'5px',
+        borderRightWidth:'1px',
+        borderRightColor:'black',
+        borderStyle:'solid',
+        justifyContent:'center'
     },
     inputQty :{
-        width:'40%'
+        width:'40%',
+        height:'75%',
+        padding:'5px',
+        borderRightWidth:'1px',
+        borderRightColor:'black',
+        borderStyle:'solid',
+        justifyContent:'center'
+    },
+    deleteIcon:{
+      width:'10%'
+    },
+    addMoreBtn:{
+      alignItems:"center"
+    },
+    btnPrimary:{
+      width:'200px',
+      padding:'5px',
+      justifyContent:'center',
+      alignItems:'center',
+      border:'2px solid black',
+      borderRadius:'5px',
+      backgroundColor:'dodgerblue',
+      fontWeight:'bold',
+      color:'white',
+    },
+    stickyFooter:{
+      marginTop: 'auto'
     }
 })
 function CreateOrder({navigation}) {
@@ -46,21 +94,31 @@ function CreateOrder({navigation}) {
     //     setOrder([initialOrder])
     // })
     return (
-    <>
-    {order.map((item, index) => {
-        return (          
-          <View key = {Math.random()}style={styles.orderRow}>
-                <OrderRow index = {index} item = {item} saveItem={saveItem}/>
-                <DeleteOrder index = {index} deleteItem={deleteItem}/>
-          </View>
-        );
-      })}
-      <TouchableOpacity onPress={() => addOrder()}>
-        <Text style={styles.clickableText}>
-          Add more
-        </Text>
-      </TouchableOpacity>
-      </>)
+    <View style={styles.container}>
+      <ScrollView style={styles.orderContainer}>
+        {order.map((item, index) => {
+            return (          
+              <View key = {Math.random()}style={styles.orderRow}>
+                    <OrderRow index = {index} item = {item} saveItem={saveItem}/>
+                    <DeleteOrder index = {index} deleteItem={deleteItem}/>
+              </View>
+            );
+          })}
+          <TouchableOpacity style = {styles.addMoreBtn}onPress={() => addOrder()}>
+            <IconButton
+              icon="plus-circle"
+              color={Colors.grey900}
+              size={30}
+              onPress={() => console.log('Pressed')}
+            />
+          </TouchableOpacity>
+      </ScrollView>
+      <View style = {styles.stickyFooter}>
+        <TouchableOpacity style = {styles.btnPrimary} onPress={() => addOrder()}>
+          Save &amp; Continue
+        </TouchableOpacity>
+      </View>
+      </View>)
 //   return (
 //     <View>
 //       <FlatList
@@ -78,52 +136,42 @@ function CreateOrder({navigation}) {
 //   );
 }
 
-
-
-const renderItem = ({item, index}) => {
-  return (
-    <View>
-      <SpeakerList
-        id={index}
-        name={item.name}
-        bio={item.bio}
-        session={item.sessions[0].name}
-      />
-    </View>
-  );
-};
-
-
 const OrderRow = ({index, item, saveItem}) => {
     const [itemName, setItemName] = React.useState(item.name)
     const [itemQty, setItemQty] = React.useState(item.qty)
     return (
         <>
-            <TextInput
-                style={styles.inputItem}
-                value={itemName}
-                onChangeText={(text) => setItemName(text)}
-                placeholder={'Enter Item name'}
-                onBlur = {()=>saveItem(index,itemName,'name')}
-            />
-            <TextInput
-                style={styles.inputQty}
-                value={itemQty}
-                onChangeText={(text) => setItemQty(text)}
-                placeholder={'Quantity'}
-                onBlur = {()=>saveItem(index,itemQty,'qty')}
-            /> 
-            
-        
-    </>
+            <View style={styles.inputItem}>
+              <TextInput
+                  value={itemName}
+                  onChangeText={(text) => setItemName(text)}
+                  placeholder={'Enter Item name'}
+                  onBlur = {()=>saveItem(index,itemName,'name')}
+              />
+            </View>
+            <View style={styles.inputQty}>
+              <TextInput
+                  value={itemQty}
+                  onChangeText={(text) => setItemQty(text)}
+                  placeholder={'Quantity'}
+                  onBlur = {()=>saveItem(index,itemQty,'qty')}
+              /> 
+            </View>
+        </>
     );
   };
 const DeleteOrder = ({index,deleteItem}) => {
     return (
-    <TouchableOpacity onPress={() => deleteItem(index)}>
-        <Text style={styles.clickableText}>
+    <TouchableOpacity style = {styles.deleteIcon} onPress={() => deleteItem(index)}>
+      <IconButton
+        icon="delete"
+        color={Colors.grey900}
+        size={20}
+        onPress={() => console.log('Pressed')}
+      />
+        {/* <Text style={styles.clickableText}>
           Delete
-        </Text>
+        </Text> */}
       </TouchableOpacity>
     );
   };
